6526fdfe92875acb62b688498cfaa63aec4529eb
itsar01 <tsarov25@gmail.com>
2023-01-24 16:52:28 +0200

    Add controller

--- cubicle/src/routes.js
+++ cubicle/src/routes.js
@@ -4,13 +4,10 @@
 
 const router = require("express").Router();
 const cubeController = require("./controllers/cubeController.js");
-router.get("/", (req, res) => {
-  res.render("index");
-});
+const homeController = require("./controllers/homeController.js");
+router.get("/", homeController.getHomePage);
 
-router.get("/about", (req, res) => {
-  res.render("about");
-});
+router.get("/about", homeController.getAboutPage);
 
 // router.get("/create", (req, res) => {
 //   res.render("create");
