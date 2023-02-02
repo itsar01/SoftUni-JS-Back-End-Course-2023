273af17a69a6cfb9494e57fe9cce59fdf8a3a91c
Ivaylo Tsarov <tsarov25@gmail.com>
2023-02-02 21:57:26 +0200

    Send cookie to client

--- cubicle/src/controllers/authController.js
+++ cubicle/src/controllers/authController.js
@@ -13,7 +13,7 @@ router.post("/login", async (req, res) => {
 
   try {
     const token = await authService.login(username, password);
-    console.log(token);
+    res.cookie("auth", token, { httpOnly: true });
   } catch (err) {
     console.log(err);
     return res.redirect("/");
