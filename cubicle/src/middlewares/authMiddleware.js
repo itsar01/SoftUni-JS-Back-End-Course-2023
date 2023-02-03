854d7ba142eca15ceb6d5567c13bcbaf25079063
Ivaylo Tsarov <tsarov25@gmail.com>
2023-02-03 21:15:13 +0200

    add auth middleware

--- cubicle/src/middlewares/authMiddleware.js
+++ cubicle/src/middlewares/authMiddleware.js
@@ -10,11 +10,20 @@ exports.authentication = async (req, res, next) => {
       const decodedToken = await jwt.verify(token, config.SECRET);
 
       req.user = decodedToken;
+      req.isAuthenticated = true;
     } catch (err) {
       console.log(err.message);
       res.clearCookie("auth");
       res.redirect("/404");
     }
+  }
+
+  next();
+};
+
+exports.isAuthenticated = (req, res, next) => {
+  if (!req.isAuthenticated) {
+    res.redirect("/login");
   }
 
   next();
--- cubicle/src/routes.js
+++ cubicle/src/routes.js
@@ -7,6 +7,7 @@ const cubeController = require("./controllers/cubeController.js");
 const homeController = require("./controllers/homeController.js");
 const accessoryController = require("./controllers/accessoryController.js");
 const authController = require("./controllers/authController.js");
+const { isAuthenticated } = require("./middlewares/authMiddleware.js");
 
 router.get("/", homeController.getHomePage);
 router.get("/about", homeController.getAboutPage);
@@ -17,8 +18,8 @@ router.get("/404", homeController.getErrorPage);
 
 router.use("/", authController);
 
-router.get("/cubes/create", cubeController.getCreateCube);
-router.post("/cubes/create", cubeController.postCreateCube);
+router.get("/cubes/create", isAuthenticated, cubeController.getCreateCube);
+router.post("/cubes/create", isAuthenticated, cubeController.postCreateCube);
 router.get("/cubes/:cubeId/details", cubeController.getDetails);
 router.get("/cubes/:cubeId/attach", cubeController.getAttachAccessory);
 router.post("/cubes/:cubeId/attach", cubeController.postAttachAccessory);
