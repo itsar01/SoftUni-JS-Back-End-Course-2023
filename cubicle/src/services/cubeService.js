537679792907eccc0f221e40c109e39da6b8f4de
Ivaylo Tsarov <tsarov25@gmail.com>
2023-02-04 14:09:21 +0200

    Edit cube

--- cubicle/src/services/cubeService.js
+++ cubicle/src/services/cubeService.js
@@ -1,3 +1,6 @@
 const Cube = require("../models/Cube.js");
 
 exports.getOne = (cubeId) => Cube.findById(cubeId);
+
+exports.update = (cubeId, data) =>
+  Cube.findByIdAndUpdate(cubeId, data, { runValidators: true });
